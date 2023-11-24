2bd8cc353aed66b2b3211db39bb91b46643b10da
fadyshenoda090 <fady.shenouda@com.svu.edu.eg>
2023-11-24 14:45:38 +0200

    ay 3abath

---
+++ lib/presentation/screens/cart_view/cart_product_box.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CartProductBox extends StatefulWidget {
+  final int index;
+  const CartProductBox({super.key, required this.index});
+
+  @override
+  State<CartProductBox> createState() => _CartProductBoxState();
+}
+
+class _CartProductBoxState extends State<CartProductBox> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
