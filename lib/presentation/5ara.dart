b8177dacd5c520415d20703e5bad6f38828e681d
fadyshenoda090 <fady.shenouda@com.svu.edu.eg>
2023-12-02 13:09:24 +0200

    adding dummy file

---
+++ lib/presentation/5ara.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Kak extends StatefulWidget {
+  const Kak({super.key});
+
+  @override
+  State<Kak> createState() => _KakState();
+}
+
+class _KakState extends State<Kak> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
