ba88545b47abf5b12d5ff4f0a66c277c7d490897
fadyshenoda090 <fady.shenouda@com.svu.edu.eg>
2023-12-02 15:16:23 +0200

    dummy file

---
+++ lib/domain/model/kak.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class kak extends StatefulWidget {
+  const kak({super.key});
+
+  @override
+  State<kak> createState() => _kakState();
+}
+
+class _kakState extends State<kak> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
